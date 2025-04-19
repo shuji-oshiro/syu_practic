c0ccede3d5a165c9191eae599e4b6f7bc8733631
shuji-oshiro <syutv117@gmail.com>
2025-04-19 11:16:06 +0900

    テストコード勉強中

---
+++ project/ok_newitem_analysis/app/src/outprint.py
@@ -0,0 +1,11 @@
+import os
+from .testapp.gologic import gogo, gogo2
+
+def getData(x):
+    return gogo(x)
+
+def getData2(x):
+    return gogo2(x)
+
+
+
---
+++ project/ok_newitem_analysis/app/src/testapp/gologic.py
@@ -0,0 +1,10 @@
+
+def gogo(value):
+    value = value + 10
+    return value
+
+def gogo2(value):
+    value = value + 20
+    return value
+
+
